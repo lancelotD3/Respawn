bfcf0ec89349c33101390e25dfe4eca553cbdda8
Jarod <jarsengkeoPro@gmail.com>
2023-07-08 03:16:06 +0200

    ldbrick - coin

---
+++ Respawn/Assets/Scripts/LDBricks/Coin.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class Coin : LDBrick
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SetState(true);
+    }
+
+    public void SetState(bool state)
+    {
+        bFinished = state;
+        Vector4 c = GetComponentInChildren<SpriteRenderer>().color;
+        GetComponentInChildren<SpriteRenderer>().color = new Vector4(c.x, c.y, c.z, state ? 1f : 0.3f);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(Coin))]
+public class CoinEditor : Editor
+{
+    private Coin self;
+
+    private void OnEnable()
+    {
+        self = (Coin)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.LabelField("ATTENTION, IL FAUT MODIFER N'IMPORTE QUEL TRUC POUR ENREGISTRER");
+        if (GUILayout.Button("Enable/Disable"))
+        {
+            self.SetState(!self.bFinished);
+        }
+    }
+}
+#endif
---
+++ Respawn/Assets/Scripts/LDBricks/LDBrick.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class LDBrick : MonoBehaviour
+{
+    public bool bFinished = false;
+}
