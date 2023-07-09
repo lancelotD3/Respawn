f0803920fefb152c52f2b1d33988faf4c9084aad
Marius <m.rabin@student.isartdigital.com>
2023-07-09 18:55:32 +0200

    la last

--- Respawn/Assets/Scripts/LDBricks/DeadEnemy.cs
+++ Respawn/Assets/Scripts/LDBricks/DeadEnemy.cs
@@ -66,11 +66,13 @@ public class DeadEnemy : LDBrick
                     shooterObject.SetActive(false);
                     break;
                 case EnemyType.GLOOPY:
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/enemies/invoke_gloopy");
                     tombObject.SetActive(false);
                     gloopyObject.SetActive(true);
                     shooterObject.SetActive(false);
                     break;
                 case EnemyType.SHOOTER:
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/enemies/invoke_tower");
                     tombObject.SetActive(false);
                     gloopyObject.SetActive(false);
                     shooterObject.SetActive(true);
